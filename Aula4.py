ace06bdd3e696cfd6747b0f465b9dd4f83025ab3
jorgelrt <jorgelrt@gmail.com>
2021-11-01 17:20:21 -0300

    laços de repetição - for e while

---
+++ Aula4.py
@@ -0,0 +1,10 @@
+#Aprofundar nas sintaxes básicas
+#Aprender a utilizar laços de repetição
+#Criar programas com condicional e laços de repetição
+
+#FOR com RANGE - imprimir os 100 primeiros números
+
+for x in range(100): #obtém de zero à 99
+    print(x)
+
+
