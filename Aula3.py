2a28fefb3f77adb004af30ad5b031a60070fb288
jorgelrt <jorgelrt@gmail.com>
2021-11-01 16:09:01 -0300

    condicional if e operadores lógicos

---
+++ Aula3.py
@@ -0,0 +1,10 @@
+#Aprofundar nas sintaxes
+#Aprender a utilizar condicionais
+#Aprender a utilizar operadores lógicos
+
+a = int(input("Informe o 1º valor: "))
+b = int(input("Informe o 2º valor: "))
+
+if a > b:
+    print('O maior número é: {}'.format(a))
+print('Final do programa')
