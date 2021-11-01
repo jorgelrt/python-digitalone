ff7a85d465f1b055f5cbab3ce548bcee956fabfc
jorgelrt <jorgelrt@gmail.com>
2021-11-01 16:13:40 -0300

    if,elif e else e operador lógico and

--- Aula3.py
+++ Aula3.py
@@ -4,7 +4,12 @@
 
 a = int(input("Informe o 1º valor: "))
 b = int(input("Informe o 2º valor: "))
+c = int(input('Informe o 3º valor: '))
 
-if a > b:
+if a > b and a > c:
     print('O maior número é: {}'.format(a))
+elif b > a and b > c:
+    print('O maior número é: {}'.format(b))
+else:
+    print('O maior número é: {}'.format(c))
 print('Final do programa')
