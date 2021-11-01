e02b6e60e9e2c1d7b236b8e3a84b7629f99fe0bb
jorgelrt <jorgelrt@gmail.com>
2021-11-01 14:37:38 -0300

    fase1

---
+++ aula2.py
@@ -0,0 +1,16 @@
+#Aprofundar nas sintaxes básicas
+#Aprender variáveis em Python
+#Realizar interações com usuário
+
+a = 10
+b = 6
+soma = a+b
+subtracao = a-b
+multiplicacao = a*b
+divisao = a/b
+resto = a%b
+print(soma)
+print(subtracao)
+print(multiplicacao)
+print(divisao)
+print(resto)
